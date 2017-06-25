6b9240c5e2cae161b0e890642be20db31147fe1b
Alexander Leontiev <aleontiev00@gmail.com>
2017-06-25 16:15:56 -0700

    Branching out of coreFeature/SQL to coreFeature/API and adding a control

---
+++ API/BattleShipsAPI/BattleShipsAPI/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BattleShipsAPI.Controllers
+{
+    public class UserController
+    {
+    }
+}
