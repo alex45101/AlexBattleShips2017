4f7480f4deab990b98e2fc9800a5094ad663432a
Alexander Leontiev <aleontiev00@gmail.com>
2017-06-26 11:34:52 -0700

    test

--- API/BattleShipsAPI/BattleShipsAPI/Controllers/UserController.cs
+++ API/BattleShipsAPI/BattleShipsAPI/Controllers/UserController.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 
 namespace BattleShipsAPI.Controllers
-{
+{    
     public class UserController
     {
     }
