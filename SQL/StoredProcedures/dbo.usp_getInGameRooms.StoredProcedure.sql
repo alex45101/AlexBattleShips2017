561ac6259f0c0e4d0e6986f70abdf15719e19725
Alexander Leontiev <aleontiev00@gmail.com>
2017-06-27 20:56:11 -0700

    updated setUser and setRoom to work with API

--- SQL/StoredProcedures/dbo.usp_getInGameRooms.StoredProcedure.sql
+++ SQL/StoredProcedures/dbo.usp_getInGameRooms.StoredProcedure.sql
@@ -1,6 +1,6 @@
 USE [AlexLeontievBattleships2017]
 GO
-/****** Object:  StoredProcedure [dbo].[usp_getInGameRooms]    Script Date: 6/27/2017 3:33:21 PM ******/
+/****** Object:  StoredProcedure [dbo].[usp_getInGameRooms]    Script Date: 6/27/2017 8:55:23 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
--- SQL/StoredProcedures/dbo.usp_setUser.StoredProcedure.sql
+++ SQL/StoredProcedures/dbo.usp_setUser.StoredProcedure.sql
@@ -1,21 +1,28 @@
 USE [AlexLeontievBattleships2017]
 GO
-/****** Object:  StoredProcedure [dbo].[usp_setUser]    Script Date: 6/27/2017 3:33:21 PM ******/
+/****** Object:  StoredProcedure [dbo].[usp_setUser]    Script Date: 6/27/2017 8:55:23 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
 
+
+
 CREATE PROC [dbo].[usp_setUser]
 	@Username nvarchar(20)
 AS
+
 DECLARE @id uniqueidentifier = NEWID()
 
 INSERT Users
 VALUES(@Username, @id)
 
-SELECT * FROM vw_Users
-WHERE Username = @Username AND PublicUserId = @id
+SELECT * 
+FROM vw_Users
+WHERE	Username = @Username
+AND		PublicUserId = @id
+
+
 
 GO
